eaec3682b6eccd620dd38eddee590ea2cb028723
KawinM23 <kawin.m235@gmail.com>
2024-04-26 14:35:45 +0700

    Fix res status for addReservation

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -107,7 +107,7 @@ exports.addReservation = async (req, res, next) => {
 
     const reservation = await Reservation.create(req.body);
 
-    res.status(200).json({ success: true, data: reservation });
+    res.status(201).json({ success: true, data: reservation });
   } catch (error) {
     console.log(error);
     return res
